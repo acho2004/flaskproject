bfd223f9e1d82474195d200ca67aca76996555c1
hunet <alexcho0514@gmail.com>
2022-08-03 09:56:49 +0900

    index now shows most recent MBTI self test result

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask import g
 from flask import render_template
 
 
@@ -32,7 +33,28 @@ def create_app(test_config=None):
 
     @app.route('/index')
     def index():
-        return render_template('index.html')
+        db = auth.get_db()
+        selfassessments = db.execute(
+            'SELECT u.email, guess_MBTI_EI, guess_MBTI_SN, guess_MBTI_TF, guess_MBTI_JP'
+            ' FROM stest p JOIN user u ON p.author_id = u.id'
+            ' ORDER BY created DESC'
+        ).fetchall()
+        found = False
+        selfguess = ""
+        for x in selfassessments:
+            if(x[0] == g.user['email']):
+                found = True
+                break
+        if not found:
+            selfguess = "N/A"
+        else:
+            selfguess = selfguess + "E" if x[1] > 0 else selfguess + "I"
+            selfguess = selfguess + "S" if x[2] > 0 else selfguess + "N"
+            selfguess = selfguess + "T" if x[3] > 0 else selfguess + "F"
+            selfguess = selfguess + "J" if x[4] > 0 else selfguess + "P"
+
+
+        return render_template('index.html', selfguess=selfguess)
 
     from . import db
     db.init_app(app)
