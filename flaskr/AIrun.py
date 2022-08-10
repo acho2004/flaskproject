0ab514090ab32470f4d0e7f130d0fdb1e6f45d8d
hunet <alexcho0514@gmail.com>
2022-08-10 13:52:22 +0900

    airun now takes last 3 results not all

--- flaskr/AIrun.py
+++ flaskr/AIrun.py
@@ -2,6 +2,8 @@ import time
 
 import sqlite3
 
+from mbti_analyzer.main import main
+
 db = sqlite3.connect(
     '../instance/flaskr.sqlite',
     detect_types=sqlite3.PARSE_DECLTYPES,
@@ -14,7 +16,7 @@ def keeprunning():
     while True:
         time.sleep(5)
 
-        x = ""
+        responses = ""
         users = db.execute(
             'SELECT * FROM hunet_members WHERE updated = 0'
         ).fetchall()
@@ -25,11 +27,22 @@ def keeprunning():
 
         for user in users:
             print(user['name'])
-            tests = db.execute(f'''SELECT * FROM ptest WHERE target_id = '{user['emp_no']}' ''')
-
+            tests = db.execute(f'''SELECT * FROM ptest WHERE target_id = '{user['emp_no']}' ORDER BY created DESC''').fetchall()
+            counter = 0
             for test in tests:
-                x += (test['sresp1'] + " " + test['sresp2'] + " " + test['sresp3'] + " " + test['sresp4'] + " " + test['sresp5'] + " / ")
-            print(x)
+                if counter == 3:
+                    break
+                else:
+                    counter += 1
+                responses += (test['sresp1'] + " " + test['sresp2'] + " " + test['sresp3'] + " " + test['sresp4'] + " " + test['sresp5'] + " / ")
+            print(responses)
+            analysis = main(responses, str(user['emp_no']))
+            print(analysis)
+            db.execute(
+                'UPDATE hunet_members SET updated = 1 WHERE emp_no = ?',
+                (user['emp_no'],)
+            )
+            db.commit()
 
 keeprunning()
 
